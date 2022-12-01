b1afac865b906132fa397f903a0bd28094bc2b65
Onai Nadapdap <onainadapdap1@gmail.com>
2022-12-01 16:38:42 +0700

    Creating controller for register endpoint

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/onainadapdap1/jwt-gin/controllers"
 	"github.com/onainadapdap1/jwt-gin/models"
 )
 
@@ -13,16 +12,14 @@ func main() {
 	// set gin router
 	r := gin.Default()
 
-	//set group of public route, endpoints that will be used as authentication 
-	public := r.Group("/api") 
+	//set group of public route, endpoints that will be used as authentication
+	public := r.Group("/api")
 
 	// public.GET("/", func(c *gin.Context) {
 	// 	c.JSON(http.StatusOK, gin.H{"data": "successfull running"})
 	// })
-	
-	public.POST("/register", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "this is the register endpoint"})
-	})
-	
+
+	public.POST("/register", controllers.Register)
+
 	r.Run(":8080")
 }
