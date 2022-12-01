4df8257631fbe2c4920d0b0e428042ff93cc3ef9
Onai Nadapdap <onainadapdap1@gmail.com>
2022-12-02 00:55:04 +0700

    save user credential to database

--- controllers/auth.go
+++ controllers/auth.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/onainadapdap1/jwt-gin/models"
 )
 
 type RegisterInput struct {
@@ -19,5 +20,23 @@ func Register(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "validated"})
+	
+	// deklarasi dan inisialisasi objek User
+	u := models.User{Username: input.Username, Password: input.Password}
+	// u.Username = input.Username
+	// u.Password = input.Password
+
+	u.Prepare()
+
+	if err := u.BeforeSave(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := u.SaveUser();
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "registration success", "data": res})
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,43 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"size:225;not null; unique" json:"username"`
 	Password string `gorm:"size:225;not null;" json:"password"`
+}
+
+// set method SaveUser milik struct User
+func (u *User) SaveUser() (*User, error) {
+	if err := DB.Create(&u).Error; err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+// set method BeforeSave milik struct User, turn password into hash
+func (u *User) BeforeSave() error {
+	// turn password into hash
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) Prepare() {
+	u.ID = 0
+	u.Username = html.EscapeString(strings.TrimSpace(u.Username))
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
 }
